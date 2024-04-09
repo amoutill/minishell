0b6f38d51692d71e599bf56e83aff92b054ba67c
blebas <blebas@student.42lehavre.fr>
2024-04-09 19:09:47 +0200

    modified magic tokenizer and add tk add char

--- src/token.c
+++ src/token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blebas <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: blebas <blebas@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 19:13:24 by blebas            #+#    #+#             */
-/*   Updated: 2024/04/09 16:39:54 by amoutill         ###   ########.fr       */
+/*   Updated: 2024/04/09 18:42:59 by blebas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,21 @@ size_t	tklst_len(t_token *tklst)
 	return (i);
 }
 
-void	tklst_addd(t_token **tklst, char *str, t_tktype type)
+void	tklst_addd(t_token **tklst, t_tktype type)
 {
 	t_token	*ptr;
 
 	if (!*tklst)
 	{
-		*tklst = malloc(sizeof(t_token));
-		(*tklst)->str = ft_strdup(str);
+		*tklst = ft_calloc(1, sizeof(t_token));
 		(*tklst)->type = type;
-		(*tklst)->next = NULL;
 		return ;
 	}
 	ptr = *tklst;
 	while (ptr->next)
 		ptr = ptr->next;
-	ptr->next = malloc(sizeof(t_token));
-	ptr->next->str = ft_strdup(str);
+	ptr->next = ft_calloc(1, sizeof(t_token));
 	ptr->next->type = type;
-	ptr->next->next = NULL;
 }
 
 void	tk_add_char(t_token *tklst, char c)
@@ -52,46 +48,43 @@ void	tk_add_char(t_token *tklst, char c)
 	char	*buf;
 	size_t	len;
 
-	//len = 0;
+	len = 0;
 	while (tklst->next)
 		tklst = tklst->next;
-	//if (!tklst->str)
-	//	tklst->str = malloc(sizeof(char) * 2);
-	//else
-	//{
-	buf = tklst->str;
-	len = ft_strlen(buf);
-	tklst->str = malloc(sizeof(char) * (len + 2));
-	ft_strlcpy(tklst->str, buf, -1);
-	//}
+	if (!tklst->str)
+		tklst->str = malloc(sizeof(char) * 2);
+	else
+	{
+		buf = tklst->str;
+		len = ft_strlen(buf);
+		tklst->str = malloc(sizeof(char) * (len + 2));
+		ft_strlcpy(tklst->str, buf, -1);
+	}
 	tklst->str[len + 0] = c;
 	tklst->str[len + 1] = '\0';
 }
 
 t_token	*magic_tokenizer(char *str)
 {
-	size_t	i;
-	size_t	start;
-	size_t	end;
-	char	*boeuf;
 	t_token	*tklst;
 
 	tklst = NULL;
 	if (!str)
 		return (NULL);
-	i = 0;
-	while (str[i])
+	while (*str)
 	{
-		while (isspace(str[i]))
-			++i;
-		start = i;
-		while (str[i] && !isspace(str[i]))
-			++i;
-		end = i;
-		boeuf = malloc(sizeof(char) * (end - start + 1));
-		ft_strlcpy(boeuf, &str[start], end - start + 1);
-		tklst_addd(&tklst, boeuf, word);
+		tklst_addd(&tklst, word);
+		while (isspace(*str))
+			++str;
+		while (*str && !is_token_end(*str))
+		{
+			//if (*str == '\'')
+			//	parse_squote(tklst, str);
+			tk_add_char(tklst, *str);
+			++str;
+		}
 	}
+	free_last(tklst);
 	return (tklst);
 }
 
