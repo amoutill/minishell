c02ce64020e7b4510a2b4168d538521220ff8a40
blebas <blebas@student.42lehavre.fr>
2024-04-17 19:47:28 +0200

    fixed buffer overflow

--- src/parse_redir.c
+++ src/parse_redir.c
@@ -6,7 +6,7 @@
 /*   By: blebas <blebas@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:09:22 by blebas            #+#    #+#             */
-/*   Updated: 2024/04/17 18:00:33 by amoutill         ###   ########.fr       */
+/*   Updated: 2024/04/17 19:46:16 by blebas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	parse_out_redir(t_token **tklst, char **cmdline)
 {
 	ft_skip_spaces(cmdline);
 	tklst_addd(tklst, out_redir);
-	++(*cmdline);
 }
 
 void	parse_out_append(t_token **tklst, char **cmdline)
@@ -24,14 +23,12 @@ void	parse_out_append(t_token **tklst, char **cmdline)
 	++(*cmdline);
 	ft_skip_spaces(cmdline);
 	tklst_addd(tklst, out_append);
-	++(*cmdline);
 }
 
 void	parse_in_redir(t_token **tklst, char **cmdline)
 {
 	ft_skip_spaces(cmdline);
 	tklst_addd(tklst, in_redir);
-	++(*cmdline);
 }
 
 void	parse_in_here_doc(t_token **tklst, char **cmdline)
@@ -39,7 +36,6 @@ void	parse_in_here_doc(t_token **tklst, char **cmdline)
 	++(*cmdline);
 	ft_skip_spaces(cmdline);
 	tklst_addd(tklst, in_here_doc);
-	++(*cmdline);
 }
 
 void	parse_pipe(t_token **tklst, char **cmdline)
@@ -48,5 +44,4 @@ void	parse_pipe(t_token **tklst, char **cmdline)
 	tklst_addd(tklst, pope);
 	if (*tklst)
 		get_last_tk(*tklst)->stop = 1;
-	++(*cmdline);
 }
